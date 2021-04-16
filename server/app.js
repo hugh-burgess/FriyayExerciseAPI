1b8ee96e7ddc520bb9e3ed4d98c77c279b2bcc9f
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-16 11:36:02 +0200

    setuup 1

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Course = require("./models/course");
+const Student = require("./models/student");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use((req, res, next) => {
+  const { method, url } = req;
+  console.log(`${method} ${url}`);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json({
@@ -10,6 +20,15 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(4000, () => {
-  console.log("Listening on http://localhost:4000");
+mongoose.connect("mongodb://localhost/Friyay16-Apr-21", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const mongodb = mongoose.connection;
+
+mongodb.on("open", () => {
+  app.listen(4000, () => {
+    console.log("Listening on http://localhost:4000");
+  });
 });
---
+++ server/models/student.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const StudentSchema = new Schema(
+  {
+    firstName: String,
+    lastName: String,
+    course: Number,
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model("Student", StudentSchema);
