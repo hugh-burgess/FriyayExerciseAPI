492ff39642367824198878e70b0c4a6688457d74
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-17 15:31:20 +0200

    error messages

--- server/app.js
+++ server/app.js
@@ -26,68 +26,84 @@ app.get("/", (req, res) => {
 // Get All Courses
 app.get("/courses", (req, res) => {
   Course.find().then((courses) => {
-    console.log(courses);
-    res.json(courses);
-    res.status(200);
+    if (![]) {
+      console.log(courses);
+      res.json(courses);
+      res.status(200);
+    } else {
+      res.status(404);
+      console.log("Not Found!");
+      res.json("Not Found!");
+    }
   });
 });
 
 // Get A Single Course
-app.get("/courses/:courseId", (req, res) =>{
-  const {courseId} = req.params
-  Course.findById(courseId)
-  .then((course) => {
-    res.status(200)
-    res.json(course)
-    console.log(course)
-  })
-})
+app.get("/courses/:courseId", (req, res) => {
+  const { courseId } = req.params;
+  Course.findById(courseId).then((course) => {
+    if (!course) {
+      res.status(404);
+      console.log("Not Found!");
+      res.json("Not Found!");
+    } else {
+      res.status(200);
+      res.json(course);
+      console.log(course);
+    }
+  });
+});
 
 // Get All Students In A Course
-app.get("/courses/:courseId/students", (req, res) =>{
-  const {courseId} = req.params
-  Student.find({"course": courseId}).then((students) => {
+app.get("/courses/:courseId/students", (req, res) => {
+  const { courseId } = req.params;
+  Student.find({ course: courseId }).then((students) => {
     if (students.course !== courseId) {
-    console.log("Not Found!")
-    res.json({error: "Not Found! Please enter a correct courseId"})
+      console.log("Not Found!");
+      res.json({ error: "Not Found! Please enter a correct courseId" });
     } else {
-    console.log(students);
-    res.json(students);
-    res.status(200);}
+      console.log(students);
+      res.json(students);
+      res.status(200);
+    }
   });
-
-})
+});
 
 // Get Single Student From Specific Course
-app.get("/courses/:courseId/students/:studentId", (req, res) =>{
-  const {studentId, courseId} = req.params
-  Student.findById(courseId && studentId).then((students) => {
-  
-    if (students.course === courseId) {
-    console.log(students);
-    res.json(students);
-    res.status(200);
-    }
-     else {
-      res.json({ error: "Pleases specify the correct course and student IDs" 
+app.get("/courses/:courseId/students/:studentId", (req, res) => {
+  const { studentId, courseId } = req.params;
+  Student.findById(courseId && studentId)
+    .then((students) => {
+      if (students.course === courseId) {
+        console.log(students);
+        res.json(students);
+        res.status(200);
+      } else {
+        res.json({
+          error: "Pleases specify the correct course and student IDs",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500);
+      res.json({
+        error: `Internal Server Error ${error}, please enter the correct ID details...`,
       });
-     }
-      }).catch((error) => {
-        res.status(500)
-res.json({error: `Internal Server Error ${error}, please enter the correct ID details...`});
-      });
-
-
-    
-  })
-
+    });
+});
 
 // Get All Students
 app.get("/students", (req, res) => {
   Student.find().then((students) => {
-    console.log(students);
-    res.json(students);
-    res.status(200);
+    if (![]) {
+      console.log(students);
+      res.json(students);
+      res.status(200);
+    } else {
+      res.status(404);
+      console.log("Not Found!");
+      res.json("Not Found!");
+    }
   });
 });
 
@@ -99,11 +115,15 @@ app.post("/courses", (req, res) => {
     res.status(400);
     res.json({ error: "Pleases specify a course by a name and type" });
   } else {
-    Course.create({ name: req.body.name, type: req.body.type, location: req.body.location }) 
+    Course.create({
+      name: req.body.name,
+      type: req.body.type,
+      location: req.body.location,
+    })
       // insert properties from Postman JSON to the JSON VSC Object
       .then((course) => {
         if (course.location === undefined) {
-          course.location = "Remote"
+          course.location = "Remote";
         }
         console.log(course);
         res.json(course); // to see the info we got from Postman, in Postmans Body Response
@@ -136,11 +156,11 @@ app.post("/courses/:courseId/students", (req, res) => {
 // Patch A Specific Course
 app.patch("/courses/:courseId", (req, res) => {
   const { courseId } = req.params;
-Course.findByIdAndUpdate(courseId, req.body, {new: true})
+  Course.findByIdAndUpdate(courseId, req.body, { new: true })
     .then((courseUpdate) => {
       res.status(200);
-      res.json(courseUpdate)
-      console.log(courseUpdate); 
+      res.json(courseUpdate);
+      console.log(courseUpdate);
     })
     .catch((error) => {
       res.status(404);
@@ -150,15 +170,14 @@ Course.findByIdAndUpdate(courseId, req.body, {new: true})
     });
 });
 
-
 // Patch A Specific Student
 app.patch("/students/:studentId", (req, res) => {
   const { studentId } = req.params;
-Student.findByIdAndUpdate(studentId, req.body, {new: true})
+  Student.findByIdAndUpdate(studentId, req.body, { new: true })
     .then((studentUpdate) => {
       res.status(200);
-      res.json(studentUpdate)
-      console.log(studentUpdate); 
+      res.json(studentUpdate);
+      console.log(studentUpdate);
     })
     .catch((error) => {
       res.status(404);
